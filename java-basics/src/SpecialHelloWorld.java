a0809ff2a236c752d9d7a43161a85aa63af79cb0
Durga Prasad Mudhuru <mdprasadeng@gmail.com>
2022-03-09 11:26:41 +0530

    noides committed

---
+++ java-basics/src/SpecialHelloWorld.java
@@ -0,0 +1,7 @@
+public class SpecialHelloWorld {
+
+  public static void main(String[] args) {
+    SpecialMessage message = new SpecialMessage();
+    System.out.println("Hello world." + message.getMessage());
+  }
+}
